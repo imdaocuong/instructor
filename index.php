8f191b57cd3088d6c14f72b6a28a5c303fb88c11
Lê Khả Đức Anh <ducanhlekha2802@gmail.com>
2021-12-27 04:25:46 +0700

    xong footer

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instructor</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <?php include('./admin/partials/navbar.php'); ?>
+
+
+    <section>
+        <div class="contact_container">
+
+        </div>
+    </section>
+    <?php include('./admin/partials/footer.php'); ?>
+    <?php include('./admin/partials/click_scroll_top.php'); ?>
+    
+
+</body>
+</html>
