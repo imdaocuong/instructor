443470784f12602879f236af46142d94eac258a3
Lê Khả Đức Anh <ducanhlekha2802@gmail.com>
2021-12-28 01:08:26 +0700

    80% navbar

--- admin/partials/navbar.php
+++ admin/partials/navbar.php
@@ -12,100 +12,97 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <body>
     <header>
-        <div class="navbar_container">
-            <div class="navbar_main">
-                <!-- NAVBAR TOP STARTS -->
-                <div class="navbar_top">
-                    <div class="navbar_row">
-                        <div class="navbar_column">
-                            <div class="navbar_social_left_side">
-                                <div class="navbar_phone_icon">
-                                    <i class="fas fa-phone"></i>
-                                    <p>need help? call us now: </p>
-                                    <a href="tel:+84123456789">(+84) 123 456 789</a>
+        <div class="navbar_main">
+            <!-- NAVBAR TOP STARTS -->
+            <div class="navbar_top">
+                <div class="navbar_row">
+                    <div class="navbar_column">
+                        <div class="navbar_social_left_side">
+                            <div class="navbar_phone_icon">
+                                <i class="fas fa-phone"></i>
+                                <p>need help? call us now: </p>
+                                <a href="tel:+84123456789">(+84) 123 456 789</a>
+                            </div>
+                            <div class="navbar_email_icon">
+                                <i class="fas fa-envelope"></i>
+                                <a href="mailto:support@cosy.com">support@instructor.com</a>
+                            </div>
+                        </div>
+                        <div class="navbar_social_right_side">
+                            <div class="navbar_social_media_icon">
+                                <div class="navbar_social_facebook">
+                                    <a href="#">
+                                        <i class="fab fa-facebook-f"></i>
+                                    </a>
                                 </div>
-                                <div class="navbar_email_icon">
-                                    <i class="fas fa-envelope"></i>
-                                    <a href="mailto:support@cosy.com">support@instructor.com</a>
+                                <div class="navbar_social_twitter">
+                                    <a href="#">
+                                        <i class="fab fa-twitter"></i>
+                                    </a>
+                                </div>
+                                <div class="navbar_social_google">
+                                    <a href="#">
+                                        <i class="fab fa-google-plus-g"></i>
+                                    </a>
+                                </div>
+                                <div class="navbar_social_linkedln">
+                                    <a href="#">
+                                        <i class="fab fa-linkedin-in"></i>
+                                    </a>
+                                </div>
+                                <div class="navbar_social_youtube">
+                                    <a href="#">
+                                        <i class="fab fa-youtube"></i>
+                                    </a>
                                 </div>
                             </div>
-                            <div class="navbar_social_right_side">
-                                <div class="navbar_social_media_icon">
-                                    <div class="navbar_social_facebook">
-                                        <a href="#">
-                                            <i class="fab fa-facebook-f"></i>
-                                        </a>
-                                    </div>
-                                    <div class="navbar_social_twitter">
-                                        <a href="#">
-                                            <i class="fab fa-twitter"></i>
-                                        </a>
-                                    </div>
-                                    <div class="navbar_social_google">
-                                        <a href="#">
-                                            <i class="fab fa-google-plus-g"></i>
-                                        </a>
-                                    </div>
-                                    <div class="navbar_social_linkedln">
-                                        <a href="#">
-                                            <i class="fab fa-linkedin-in"></i>
-                                        </a>
-                                    </div>
-                                    <div class="navbar_social_youtube">
-                                        <a href="#">
-                                            <i class="fab fa-youtube"></i>
-                                        </a>
-                                    </div>
+                            <div class="navbar_search_login">
+                                <div class="navbar_search">
+                                    <i class="fas fa-search"></i>
                                 </div>
-                                <div class="navbar_search_login">
-                                    <div class="navbar_search">
-                                        <i class="fas fa-search"></i>
-                                    </div>
-                                    <div class="navbar_login">
-                                        <i class="fas fa-user"></i>
-                                    </div>
+                                <div class="navbar_login">
+                                    <i class="fas fa-user"></i>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <!-- NAVBAR TOP ENDS -->
-
-                <!-- NAVBAR BOTTOM STARTS -->
-                <div class="navbar_bottom" id="navbar">
-                    <div class="navbar_row">
-                        <div class="navbar_column">
-                            <div class="navbar_logo_left">
-                                <p>instructor</p>
-                            </div>
-                            <!-- MENU FOR PC STARTS -->
-                            <ul class="navbar_menu_right">
-                                <li><a href=""></a>Features</li>
-                                <li><a href=""></a>Events</li>
-                                <li><a href=""></a>Gallery</li>
-                                <li><a href=""></a>Blog</li>
-                                <li><a href=""></a>Contact</li>
-                                <li><a href="facebook.com"></a>Shop</li>
-                            </ul>
-                            <!-- MENU FOR PC ENDS -->
-                        </div>
-                    </div>
-                    <!-- MENU FOR MOBILE STARTS
-                    <ul>
-
-                    </ul> -->
-                    <!-- MENU FOR MOBILE ENDS -->
-                </div>
-                <!-- NAVBAR BOTTOM ENDS -->
             </div>
+            <!-- NAVBAR TOP ENDS -->
+
+            <!-- NAVBAR BOTTOM STARTS -->
+            <div class="navbar_bottom" id="navbar">
+                <div class="navbar_row">
+                    <div class="navbar_column">
+                        <div class="navbar_logo_left">
+                            <p>instructor</p>
+                        </div>
+                        <!-- MENU FOR PC STARTS -->
+                        <ul class="navbar_menu_right">
+                            <li><a href=""></a>Features</li>
+                            <li><a href=""></a>Events</li>
+                            <li><a href=""></a>Gallery</li>
+                            <li><a href=""></a>Blog</li>
+                            <li><a href=""></a>Contact</li>
+                            <li><a href="facebook.com"></a>Shop</li>
+                        </ul>
+                        <!-- MENU FOR PC ENDS -->
+                    </div>
+                </div>
+                <!-- MENU FOR MOBILE STARTS
+                <ul>
+
+                </ul> -->
+                <!-- MENU FOR MOBILE ENDS -->
+            </div>
+            <!-- NAVBAR BOTTOM ENDS -->
+        </div>
             
 
             <!-- home section starts -->
           
             <!-- home section ends -->
-        
     </header>
-</div>
 <script type="text/javascript" src="../../js/jquery-3.6.0.min.js"></script>
 <script type="text/javascript" src="../../js/main.js"></script>
 </body>
--- index.php
+++ index.php
@@ -11,21 +11,27 @@
 <body>
     <?php include('./admin/partials/navbar.php');?>
     <div class="home_page_banner" id="home_page">
-        <div class="content">
-            <span>Welcome Instructor</span>
-            <h3>Start your Ali Human </h3>
-            <h3>Revolution today 😋</h3>
-            <p>with private practice in FPT Aptech</p>
-            <div class="home_page_click">
-                <div class="icon_click">
-                    <i class="fab fa-telegram-plane"></i>
+        <div class="banner_row">
+            <div class="banner_column">
+                <div class="content">
+                    <span>Welcome Instructor</span>
+                    <h3>Start your Ali Human </h3>
+                    <h3>Revolution today 😋</h3>
+                    <p>with private practice in FPT Aptech</p>
+                    <div class="home_page_click">
+                        <div class="icon_click">
+                            <i class="fab fa-telegram-plane"></i>
+                        </div>
+                        <a data-text="CLICK TO PLAY" href="#">CLICK TO PLAY</a>
+                    </div>
                 </div>
-                <a data-text="CLICK TO PLAY" href="#">CLICK TO PLAY</a>
+                     
             </div>
         </div>
         <div class="image">
             <video loop muted autoplay src="../instructor/image/background.mp4"></video>
-        </div>         
+        </div>    
+       
     </div>
 
 
