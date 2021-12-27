3d2c21c8eab588f42bc5477ef2d9244687064b37
Lê Khả Đức Anh <ducanhlekha2802@gmail.com>
2021-12-27 23:54:41 +0700

    70% navbar

--- index.php
+++ index.php
@@ -6,19 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Instructor</title>
     <link rel="stylesheet" href="./css/style.css">
+    
 </head>
 <body>
-    <?php include('./admin/partials/navbar.php'); ?>
-
-
-    <section>
-        <div class="contact_container">
-
+    <?php include('./admin/partials/navbar.php');?>
+    <div class="home_page_banner" id="home_page">
+        <div class="content">
+            <span>Welcome Instructor</span>
+            <h3>Start your Ali Human </h3>
+            <h3>Revolution today 😋</h3>
+            <p>with private practice in FPT Aptech</p>
+            <div class="home_page_click">
+                <div class="icon_click">
+                    <i class="fab fa-telegram-plane"></i>
+                </div>
+                <a data-text="CLICK TO PLAY" href="#">CLICK TO PLAY</a>
+            </div>
         </div>
-    </section>
+        <div class="image">
+            <video loop muted autoplay src="../instructor/image/background.mp4"></video>
+        </div>         
+    </div>
+
+
     <?php include('./admin/partials/footer.php'); ?>
     <?php include('./admin/partials/click_scroll_top.php'); ?>
-    
 
 </body>
 </html>
